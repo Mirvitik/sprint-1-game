ff51628c3f796afe68931596bbb968f0ba28589f
Mirvitik <mirvitik@vk.com>
2026-02-15 16:39:41 +0300

    UPDATED: game field updated

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -9,38 +10,89 @@ public class Main {
         int sizeBoard = 5;
         personX = 1 + sizeBoard / 2;
         personY = 1 + sizeBoard / 2;
-        String person = "\uD83E\uDDDF\u200D";
-        String monster = "\uD83E\uDDD9\u200D";
-        String gamingField = "+ —— + —— + —— +\n"
-                + "|    |    |    |\n"
-                + "+ —— + —— + —— +\n"
-                + "|    | " + monster + " |    |\n"
-                + "+ —— + —— + —— +\n"
-                + "| " + person + " |    |    |\n"
-                + "+ —— + —— + —— +";
-        boolean running = true;
+        String person = "\uD83E\uDDD9\u200D";
+        String monster = "\uD83E\uDDDF\u200D";
+        String castle = "\uD83C\uDFF0";
+
+        String leftBlock = "| ";
+        String rightBlock = "|";
+        String wall = "+ —— ".repeat(sizeBoard) + "+";
+        String[][] board = new String[sizeBoard][sizeBoard];
+        for (int y = 0; y < sizeBoard; y++) {
+            for (int x = 0; x < sizeBoard; x++) {
+                board[y][x] = "  ";
+            }
+        }
+
+        int countMonster = sizeBoard * sizeBoard - sizeBoard - 1;
+        Random r = new Random();
+        for (int i = 0; i <= countMonster; i++) {
+            board[r.nextInt(sizeBoard - 1)][r.nextInt(sizeBoard)] = monster;
+        }
+
+        int castleX = r.nextInt(sizeBoard);
+        int castleY = 0;
+        board[castleY][castleX] = castle;
         System.out.println("Ты готов начать играть в игру? (Напиши: ДА или НЕТ)");
         Scanner sc = new Scanner(System.in);
         String answer = sc.nextLine();
         switch (answer) {
             case "ДА":
-                while (running) {
-                    System.out.println(gamingField);
+                System.out.println("Выбери сложность игры(от 1 до 5):");
+                int difficultGame = sc.nextInt();
+                System.out.println("Выбранная сложность:\t" + difficultGame);
+
+                int maxStep = 2;
+                while (true) {
+                    board[personY - 1][personX - 1] = person;
+                    for (String[] raw : board) {
+                        System.out.println(wall);
+                        for (String col : raw) {
+                            System.out.print(leftBlock + col + " ");
+                        }
+                        System.out.println(rightBlock);
+                    }
+                    System.out.println(wall);
+
                     System.out.println("Введите куда будет ходить персонаж(ход возможен только по вертикали и горизонтали на одну клетку;");
                     System.out.println("Координаты персонажа - (x: " + personX + ", y: " + personY + "))");
 
                     int x = sc.nextInt();
                     int y = sc.nextInt();
+                    System.out.println(x + ", " + y);
                     if (x == personX && y == personY) {
                         System.out.println("Неккоректный ход");
                     } else if (Boolean.logicalXor(Math.abs(x - personX) == 1, Math.abs(y - personY) == 1)) {
-                        personX = x;
-                        personY = y;
-                        step += 1;
+                        if (board[y - 1][x - 1].equals("  ")) {
+                            board[personY - 1][personX - 1] = "  ";
+                            personX = x;
+                            personY = y;
+                            step++;
+                            System.out.println("Ход корректный; Новые координаты: " + personX + ", " + personY +
+                                    "\nХод номер: " + step);
+                        } else if (board[y - 1][x - 1].equals(castle)) {
+                            System.out.println("Вы прошли игру!");
+                            break;
+                        } else {
+                            personLive -= 1;
+                            if (personLive <= 0) {
+                                System.out.println("Потрачено. Игра закончиалсь.");
+                            } else if (personLive == 1) {
+                                System.out.println("Вы наткнулись на монстра. Ваши координаты: " + personX + ", " + personY +
+                                        "\nУ вас осталось: " + personLive + " жизнь");
+                            } else {
+                                System.out.println("Вы наткнулись на монстра. Ваши координаты: " + personX + ", " + personY +
+                                        "\nУ вас осталось: " + personLive + " жизней");
+                            }
+                        }
                         System.out.println("Ход корректный; Новые координаты: " +
                                 personX + ", " + personY + "\nХод номер: " + step);
                     } else {
                         System.out.println("Координаты не изменены");
+                    }
+                    if (personLive <= 0) {
+                        System.out.println("Закончились жизни. Итоги:\n" + "Сделано " + step + " шагов\n" + "Последняя координата " + personX + "," + personY);
+                        break;
                     }
                 }
                 break;
