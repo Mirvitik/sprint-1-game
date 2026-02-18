d09e5127c8589c9afc602524478d3e5e5f3b372f
Mirvitik <mirvitik@vk.com>
2026-02-18 22:13:44 +0300

    UPDATED: task function added

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,35 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
+    static boolean taskMonster() {
+        Random r = new Random();
+        int x = r.nextInt(100);
+        int y = r.nextInt(100);
+        int trueAnswer = x - y;
+        System.out.println("Реши пример: " + x + " - " + y + " = ?");
+        Scanner sc = new Scanner(System.in);
+        int ans = sc.nextInt();
+        if (trueAnswer == ans) {
+            System.out.println("Верно! Ты победил монстра");
+            return false;
+        }
+        System.out.println("Ты проиграл эту битву!");
+        return true;
+    }
+
     public static void main(String[] args) {
         int step = 0;
         int personX;
         int personY;
+
         int personLive = 3;
+
         int sizeBoard = 5;
         personX = 1 + sizeBoard / 2;
         personY = 1 + sizeBoard / 2;
-        String person = "\uD83E\uDDD9\u200D";
-        String monster = "\uD83E\uDDDF\u200D";
-        String castle = "\uD83C\uDFF0";
+        String person = "Гг";
+        String monster = "Мм";
+        String castle = "З ";
 
         String leftBlock = "| ";
         String rightBlock = "|";
@@ -60,7 +78,7 @@ public class Main {
                     int x = sc.nextInt();
                     int y = sc.nextInt();
                     System.out.println(x + ", " + y);
-                    if (x == personX && y == personY) {
+                    if ((x == personX && y == personY) || !(0 <= x && x <= sizeBoard && 0 <= y && y <= sizeBoard)) {
                         System.out.println("Неккоректный ход");
                     } else if (Boolean.logicalXor(Math.abs(x - personX) == 1, Math.abs(y - personY) == 1)) {
                         if (board[y - 1][x - 1].equals("  ")) {
@@ -74,7 +92,9 @@ public class Main {
                             System.out.println("Вы прошли игру!");
                             break;
                         } else {
-                            personLive -= 1;
+                            if (taskMonster()) {
+                                personLive -= 1;
+                            }
                             if (personLive <= 0) {
                                 System.out.println("Потрачено. Игра закончиалсь.");
                             } else if (personLive == 1) {
